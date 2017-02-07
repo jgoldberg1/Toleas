80bcaf0fffff031fc79b52d9ab8b92bd26bbc6fb
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-02-07 14:05:58 -0800

    syntax fixes. trying to debug

--- toleas.js
+++ toleas.js
@@ -4,11 +4,13 @@ var rankTwo = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "
 
 var rankThree = ["kn", "sf", "sm", "fl", "sh", "ch", "chr", "b", "d", "h", "j", "l"];
 
-var rankFour: = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
+var rankFour = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
 
-var rankFive: = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
+var rankFive = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
 
-function pickRank(rank); {
+var rank;
+
+function pickRank() {
     var randRank = Math.floor((Math.random() * 16));
 
     if (randRank == 1 || randRank == 2 || randRank == 3 || randRank == 4 || randRank == 5) {
@@ -17,7 +19,7 @@ function pickRank(rank); {
         rank = rankTwo;
     } else if (randRank == 10 || randRank == 11 || randRank == 12) {
         rank = rankThree;
-    } else if (rank == 13 || randRank == 14) {
+    } else if (randRank == 13 || randRank == 14) {
         rank = rankFour;
     } else {
         rank = rankFive;
