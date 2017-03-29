a12ca228522f38e9f6493a283ea4c1e0b963cbbe
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-03-29 14:37:13 -0700

    sdf

--- toleas.js
+++ toleas.js
@@ -1,27 +1,32 @@
 var rankOne = ["r", "s", "t", "v", "thr", "sk", "fr", "gr", "sr", "sv", "vr", "k"];
-
 var rankTwo = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "y", "w"];
-
 var rankThree = ["kn", "sf", "sm", "fl", "sh", "ch", "chr", "b", "d", "h", "j", "l"];
-
 var rankFour = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
-
 var rankFive = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
-
 var rank;
 
 function pickRank() {
     var randRank = Math.floor((Math.random() * 16));
-
     if (randRank == 1 || randRank == 2 || randRank == 3 || randRank == 4 || randRank == 5) {
         rank = rankOne;
-    } else if (randRank == 6  || randRank == 7 || randRank == 8 || randRank == 9) {
+    }
+    else if (randRank == 6 || randRank == 7 || randRank == 8 || randRank == 9) {
         rank = rankTwo;
-    } else if (randRank == 10 || randRank == 11 || randRank == 12) {
+    }
+    else if (randRank == 10 || randRank == 11 || randRank == 12) {
         rank = rankThree;
-    } else if (randRank == 13 || randRank == 14) {
+    }
+    else if (randRank == 13 || randRank == 14) {
         rank = rankFour;
-    } else {
+    }
+    else {
         rank = rankFive;
     }
+    return rank;
+}
+
+
+function randPhoneme() {
+    var randPho = rank.length;
+    document.write(randpho);
 }
--- toleasTest.js
+++ toleasTest.js
@@ -1,4 +1,4 @@
-QUnit.test("test pickRank function", function (assert) {
+QUnit.test( "test pickRank function", function( assert ) {
  var rankOne = ["r", "s", "t", "v", "thr", "sk", "fr", "gr", "sr", "sv", "vr", "k"];
 
 var rankTwo = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "y", "w"];
@@ -9,8 +9,16 @@ var rankFour = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
 
 var rankFive = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
 
-    var rank;
-
     var result = pickRank();
     var randRank = 4;
-    assert.deepEqual(result, rankOne, "We expect result to be rankOne");
+    assert.deepEqual(result, "rankOne", "We expect result to be rankOne");
+});
+
+
+    // Test for inputing digits
+QUnit.test( "Add digits test", function( assert ) {
+    Calculator.allClear();
+    Calculator.addDigit('1');
+    Calculator.addDigit('2');
+    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+});
