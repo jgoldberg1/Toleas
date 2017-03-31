6dc7e2c50176454c15f94fa0a36c21c8bb7a9a4a
Jenna M Goldberg 9 <jg32137@srcs.loc>
2017-03-31 13:32:41 -0700

    randPhonemeC and pickRankC are functional

--- toleas.js
+++ toleas.js
@@ -1,32 +1,40 @@
-var rankOne = ["r", "s", "t", "v", "thr", "sk", "fr", "gr", "sr", "sv", "vr", "k"];
-var rankTwo = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "y", "w"];
-var rankThree = ["kn", "sf", "sm", "fl", "sh", "ch", "chr", "b", "d", "h", "j", "l"];
-var rankFour = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
-var rankFive = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
-var rank;
 
-function pickRank() {
-    var randRank = Math.floor((Math.random() * 16));
-    if (randRank == 1 || randRank == 2 || randRank == 3 || randRank == 4 || randRank == 5) {
-        rank = rankOne;
+//CONSONANT CHOOSING PROCESS
+var rankOneC = ["r", "s", "t", "v", "thr", "sk", "fr", "gr", "sr", "sv", "vr", "k"];
+var rankTwoC = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "y", "w"];
+var rankThreeC = ["kn", "sf", "sm", "fl", "sh", "ch", "chr", "b", "d", "h", "j", "l"];
+var rankFourC = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
+var rankFiveC = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
+var rankC;
+
+function pickRankC() {
+    var RandRankC = Math.floor((Math.random() * 16));
+    if (RandRankC == 1 || RandRankC == 2 || RandRankC == 3 || RandRankC == 4 || RandRankC == 5) {
+        rankC = rankOneC;
     }
-    else if (randRank == 6 || randRank == 7 || randRank == 8 || randRank == 9) {
-        rank = rankTwo;
+    else if (RandRankC == 6 || RandRankC == 7 || RandRankC == 8 || RandRankC == 9) {
+        rankC = rankTwoC;
     }
-    else if (randRank == 10 || randRank == 11 || randRank == 12) {
-        rank = rankThree;
+    else if (RandRankC == 10 || RandRankC == 11 || RandRankC == 12) {
+        rankC = rankThreeC;
     }
-    else if (randRank == 13 || randRank == 14) {
-        rank = rankFour;
+    else if (RandRankC == 13 || RandRankC == 14) {
+        rankC = rankFourC;
     }
     else {
-        rank = rankFive;
+        rankC = rankFiveC;
     }
-    return rank;
+    return rankC;
+
 }
 
 
-function randPhoneme() {
-    var randPho = rank.length;
-    document.write(randpho);
+function randPhonemeC() {
+    var arrElemC = rankC[Math.ceil(Math.random() * rankC.length)];
+    ;
+ document.write(arrElemC);
 }
+
+
+
+
--- toleasTest.js
+++ toleasTest.js
@@ -1,17 +1,7 @@
-QUnit.test( "test pickRank function", function( assert ) {
- var rankOne = ["r", "s", "t", "v", "thr", "sk", "fr", "gr", "sr", "sv", "vr", "k"];
+QUnit.test( "test randPhonemeC function", function( assert ) {
 
-var rankTwo = ["thl", "dr", "kr", "gl", "skl", "f", "th", "br", "g", "n", "m", "y", "w"];
-
-var rankThree = ["kn", "sf", "sm", "fl", "sh", "ch", "chr", "b", "d", "h", "j", "l"];
-
-var rankFour = ["pl", "sn", "br", "fj", "klr", "p", "z", "chv", "chj"];
-
-var rankFive = ["dj", "snr", "krt", "krs", "kld", "klj", "blr", "wj", "zr", "x"];
-
-    var result = pickRank();
-    var randRank = 4;
-    assert.deepEqual(result, "rankOne", "We expect result to be rankOne");
+    var result = randPhoneme(rank = );
+    assert.deepEqual(result, "rankOne[1-13]", "We expect result to be rankOne");
 });
 
 
